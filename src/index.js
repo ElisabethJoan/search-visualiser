3ba87587be240cff412e0ed413c6ba00b270c09a
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2022-05-09 21:48:58 +1000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      <p>test</p>
+    </div>
+  );
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
