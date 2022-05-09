a6d96ca3f0ab347a7a90998fccbccb3554d75513
Elisabeth Joan <Elisabeth.L.Joan@gmail.com>
2022-05-10 03:20:09 +1000

    balanced binary tree with node depth

---
+++ src/binarytree.js
@@ -0,0 +1,50 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.depth = 0;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+export default class BalancedBinaryTree {
+    constructor(arr) {
+        this.root = this.arrayToTree(arr, 0, arr.length - 1, 0);
+    }
+
+    arrayToTree(arr, start, end, depth) {
+        if (start > end)
+        {
+            return null;
+        }
+        
+        let mid = parseInt((start + end) / 2);
+        let node = new Node(arr[mid]);
+
+        node.depth = depth
+
+        node.left = this.arrayToTree(arr, start, mid - 1, depth++);
+        node.right = this.arrayToTree(arr, mid + 1, end, depth++);
+
+        return node;
+    }
+
+    toNodeArray() {
+        let nodeArray = [];
+
+        function preOrderTraversal(node) {
+            if (node == null)
+            {
+                return;
+            }
+
+            nodeArray.push(node)
+            preOrderTraversal(node.left);
+            preOrderTraversal(node.right);
+        }
+
+        preOrderTraversal(this.root)
+
+        return nodeArray;
+    }
+}
